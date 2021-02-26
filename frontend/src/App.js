9940917e40957ddf3d23eb05555177c5932a3a7f
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-02-26 17:39:56 -0500

    navigation ul no bullets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import PublicHomePage from "./components/PublicHomePage";
+
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +20,12 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path="/">
+            <PublicHomePage />
+          </Route> 
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
---
+++ frontend/src/components/PublicHomePage/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./PublicHomePage.css";
+
+export default function PublicHomePage() {
+    return (
+        <div>
+            <h1>Oxillary.</h1>
+        </div>
+    );
+}
